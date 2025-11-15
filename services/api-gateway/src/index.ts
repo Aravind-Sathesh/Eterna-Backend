7479db9ac4c038b51b01dbf4318327255f6808de
Aravind Sathesh <aravind.sathesh@gmail.com>
2025-11-15 20:03:33 +0530

    feat: add basic frontend

--- services/api-gateway/src/index.ts
+++ services/api-gateway/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import tokenRoutes from './routes/tokenRoutes';
 import { redisClient, isRedisConnected } from '@eterna/redis-client';
@@ -8,6 +9,7 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (_req, res) => {
